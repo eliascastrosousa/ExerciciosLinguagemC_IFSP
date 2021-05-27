f524ee8da3baba0f5aab007c29595a1e8c3df978
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-05-26 22:44:55 -0300

    up

---
+++ caractere.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main() {
+int num;
+printf("Digite um valor inteiro: ");
+scanf("%d",&num);
+
+printf("O caractere correspondente ao valor %d na tabela ascii e o %c",num,(char)num);
+
+}
