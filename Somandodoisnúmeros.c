e048ea82c53590435a9c65e1ef60f51dff9f26ac
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-06-03 12:50:43 -0300

    commit

---
+++ Somandodoisnúmeros.c
@@ -0,0 +1,14 @@
+int main(int argc, char const *argv[])
+{
+    int n1,n2;
+
+    printf("Digite um numero: ");
+    scanf("%d",&n1);
+
+    printf("Digite outro numero: ");
+    scanf("%d",&n2);
+
+    printf("A soma dos dois numeros é igual a: %d",n1+n2);
+
+    return 0;
+}
