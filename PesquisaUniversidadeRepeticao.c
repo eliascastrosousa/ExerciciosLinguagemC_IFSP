b9ac61258aeafa1062c797207969bd2068814964
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-07-01 20:31:47 -0300

    commit

--- PesquisaUniversidadeRepeticao.c
+++ PesquisaUniversidadeRepeticao.c
@@ -11,13 +11,25 @@ Ex.: 2, 3, 11, 12, 21, 22, 23 = a) 28%;     b) 28%;     c) 42% */
 main(){
 setlocale(LC_ALL, "portuguese");
 
-int count=1, finish, quantity;
-printf("SEJA BEM VINDO \n\n");
-while(quantity != int){
-    printf("\n\nAluno %d, Digite quantas vezes você utilizou o Restaurante no ultimo mês:\nOu digite 0 para encerrar: ", count);
-    scanf("%f", &quantity);
+
+float a_avg, b_avg, c_avg,quantity, count=1,classe_a=0, classe_b=0, classe_c=0;
+
+while(count<=5){
+    printf("\n\nAluno %.0f, Digite quantas vezes você utilizou o Restaurante no ultimo mês: ", count);
+    scanf("%f",&quantity);
     count++;
+    if(quantity > 0 && quantity <=10 ){
+        classe_a++;
+    }else if (quantity > 10 && quantity <=15){
+        classe_b++;
+    }else if (quantity > 15 && quantity <=20){
+        classe_c++;
+    }else{
+        printf("valor invalido");
+    }
 }
-
-
+a_avg = classe_a/count*100;
+b_avg = classe_b/count*100;
+c_avg = classe_c/count*100;
+printf("\nCATEGORA [A]: %.2f%%\nCATEGORA [B]: %.2f%%\nCATEGORA [C]: %.2f%%\n", a_avg , b_avg , c_avg );
 }
