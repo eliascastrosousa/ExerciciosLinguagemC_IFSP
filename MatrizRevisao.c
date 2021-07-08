27fd8b351cbd2461fd0076ef18773aea5a7383bb
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-07-08 10:50:56 -0300

    commit

---
+++ MatrizRevisao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+#define TAM 3 //CONSTANTE
+
+main(){
+
+int matriz[TAM][TAM],linha, coluna;
+setlocale(LC_ALL, "portuguese");
+
+for(linha=0; linha < TAM; linha++){
+
+}
+}
